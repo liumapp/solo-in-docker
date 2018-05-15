9fc0f81c924242ab06da602626c6a3f30fb03334
liumapp <liumapp.com@gmail.com>
2018-05-15 16:05:57 +0800

    ready to config docker-compose

---
+++ build-image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      liumapp                              $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  email:    liumapp.com@gmail.com                          $'
+echo '$  homePage: http://www.liumapp.com                         $'
+echo '$  Github:   https://github.com/liumapp                     $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+
+docker pull mysql:5.5.60
+
+docker pull nginx:1.13
+
+docker pull tomcat:8.0
+
