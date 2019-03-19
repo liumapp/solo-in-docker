fc56580d474b922f199a6b7026969f450998b5ee
liumapp <liumapp.com@gmail.com>
2019-03-19 15:34:40 +0800

    add data-transporter module

---
+++ data-transporter/src/main/java/com/liumapp/solo/transporter/App.java
@@ -0,0 +1,13 @@
+package com.liumapp.solo.transporter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
