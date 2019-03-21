ec33753b6b589662c1782d70e1ff41d0d1b7112b
liumapp <liumapp.com@gmail.com>
2019-03-21 20:32:31 +0800

    update archivedate article class

--- data-transporter/src/main/java/com/liumapp/solo/transporter/services/impl/ArchivedateArticleHandler.java
+++ data-transporter/src/main/java/com/liumapp/solo/transporter/services/impl/ArchivedateArticleHandler.java
@@ -1,9 +1,15 @@
 package com.liumapp.solo.transporter.services.impl;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.liumapp.solo.transporter.contents.JsonFileContents;
+import com.liumapp.solo.transporter.db.entity.B3SoloArchivedateArticle;
 import com.liumapp.solo.transporter.db.mapper.B3SoloArchivedateArticleMapper;
 import com.liumapp.solo.transporter.services.HandDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
 
 /**
  * file ArchivedateArticleHandler.java
@@ -19,9 +25,17 @@ public class ArchivedateArticleHandler implements HandDataService {
     @Autowired
     private B3SoloArchivedateArticleMapper mapper;
 
+    @Autowired
+    private JsonFileContents jsonFileContents;
+
     @Override
     public void handle() {
-
+        JSONArray archivedates = jsonFileContents.getArchivedateArticle();
+        B3SoloArchivedateArticle archivedateArticle = new B3SoloArchivedateArticle();
+        for (Iterator iterator = archivedates.iterator(); iterator.hasNext();) {
+            JSONObject archivedate = (JSONObject) iterator.next();
+            
+        }
     }
 
 }
