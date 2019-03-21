c0bc855842c6e531de39cc4867b1fa0ca2e74229
liumapp <liumapp.com@gmail.com>
2019-03-21 14:18:30 +0800

    update article handler

--- data-transporter/src/main/java/com/liumapp/solo/transporter/services/impl/ArticleHandler.java
+++ data-transporter/src/main/java/com/liumapp/solo/transporter/services/impl/ArticleHandler.java
@@ -1,15 +1,9 @@
 package com.liumapp.solo.transporter.services.impl;
 
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.liumapp.solo.transporter.contents.JsonFileContents;
-import com.liumapp.solo.transporter.domain.B3SoloArticle;
-import com.liumapp.solo.transporter.domain.B3SoloArticleExample;
-import com.liumapp.solo.transporter.domain.B3SoloArticleWithBLOBs;
-import com.liumapp.solo.transporter.mapper.B3SoloArticleMapper;
 import com.liumapp.solo.transporter.services.HandDataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Iterator;
@@ -28,21 +22,16 @@ public class ArticleHandler implements HandDataService {
     @Autowired
     private JsonFileContents jsonFileContents;
 
-    @Autowired
-    private B3SoloArticleMapper b3SoloArticleMapper;
-
     @Override
     public void handle() {
         JSONArray articles = jsonFileContents.getArticle();
-        B3SoloArticleWithBLOBs b3SoloArticle = new B3SoloArticleWithBLOBs();
-        for (Iterator iterator = articles.iterator(); iterator.hasNext();) {
-            JSONObject article = (JSONObject) iterator.next();
-            b3SoloArticle.setoId(article.get("oId").toString());//主键
-            b3SoloArticle.setArticleTitle(article.get("articleTitle").toString());//文章标题
-            b3SoloArticle.setArticleAbstract(article.get("articleAbstract").toString());//文章摘要
+//        B3SoloArticleWithBLOBs b3SoloArticle = new B3SoloArticleWithBLOBs();
+//        for (Iterator iterator = articles.iterator(); iterator.hasNext();) {
+//            JSONObject article = (JSONObject) iterator.next();
+//            b3SoloArticle.setoId(article.get("oId").toString());//主键
+//            b3SoloArticle.setArticleTitle(article.get("articleTitle").toString());//文章标题
+//            b3SoloArticle.setArticleAbstract(article.get("articleAbstract").toString());//文章摘要
 //            b3SoloArticle.setarticleabs
-        }
     }
-
 
 }
