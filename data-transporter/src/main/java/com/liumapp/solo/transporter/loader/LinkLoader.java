3ca33e6e1b1f015f1da4a870da421908c3590e8b
liumapp <liumapp.com@gmail.com>
2019-03-21 09:32:32 +0800

    add link loader class

---
+++ data-transporter/src/main/java/com/liumapp/solo/transporter/loader/LinkLoader.java
@@ -0,0 +1,40 @@
+package com.liumapp.solo.transporter.loader;
+
+import com.alibaba.fastjson.JSON;
+import com.liumapp.qtools.file.basic.FileTool;
+import com.liumapp.solo.transporter.contents.JsonFileContents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * file LinkLoader.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/3/21
+ */
+@Component
+@Scope("prototype")
+public class LinkLoader implements Runnable {
+    private static final Logger Logger = LoggerFactory.getLogger(ArticleLoader.class);
+
+    @Autowired
+    private JsonFileContents jsonFileContents;
+
+    @Override
+    public void run() {
+        Logger.info("LinkLoader begin...");
+        try {
+            String filepath = this.getClass().getResource("/json/b3_solo_link.json").getPath();
+            jsonFileContents.setLink(JSON.parseObject(FileTool.readFileAsString(filepath)));
+            Logger.info("LinkLoader done...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
