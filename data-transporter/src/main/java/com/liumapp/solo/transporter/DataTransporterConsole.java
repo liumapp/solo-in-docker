d3791f0495f1d7a46d7d8f9a5bcfb1e5663175b5
liumapp <liumapp.com@gmail.com>
2019-03-21 20:24:13 +0800

    add archivedate article handler class

--- data-transporter/src/main/java/com/liumapp/solo/transporter/DataTransporterConsole.java
+++ data-transporter/src/main/java/com/liumapp/solo/transporter/DataTransporterConsole.java
@@ -51,23 +51,12 @@ public class DataTransporterConsole implements CommandLineRunner {
         Thread.sleep(3000);
         textIO = new TextIO(systemTextTerminal);
         Boolean isDo = textIO.newBooleanInputReader().read("要开始导入数据吗？");
-        DataEnums datas = textIO.newEnumInputReader(DataEnums.class).read("先插入文章还是评论？");
-
-        switch (datas) {
-            case Article:
-                this.handleArticle();
-                break;
-            case Comment:
-                this.handleComment();
-                break;
-            default:
+        if (isDo) {
+            this.beginHandData();
         }
     }
 
-    /**
-     * 处理文章数据，插入solo数据库中
-     */
-    private void handleArticle () {
+    private void beginHandData () {
         int length = jsonFileContents.getArticle().size();
         if (length == 0) {
             Logger.error("未能捕获到文章信息，结束");
@@ -76,13 +65,9 @@ public class DataTransporterConsole implements CommandLineRunner {
         Logger.info("开始处理文章插入逻辑");
         Logger.info("首先插入article数据");
 //        articleHandler.handle();
+        Logger.info("开始导入文章与创建时间的关联数据");
+
 
     }
 
-    /**
-     * 处理评论数据
-     */
-    private void handleComment () {
-
-    }
 }
---
+++ data-transporter/src/main/java/com/liumapp/solo/transporter/services/impl/ArchivedateArticleHandler.java
@@ -0,0 +1,22 @@
+package com.liumapp.solo.transporter.services.impl;
+
+import com.liumapp.solo.transporter.services.HandDataService;
+import org.springframework.stereotype.Service;
+
+/**
+ * file ArchivedateArticleHandler.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/3/21
+ */
+@Service
+public class ArchivedateArticleHandler implements HandDataService {
+
+    @Override
+    public void handle() {
+
+    }
+
+}
