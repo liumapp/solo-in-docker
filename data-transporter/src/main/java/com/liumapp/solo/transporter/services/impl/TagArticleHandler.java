602783081740eaffefd7e1f656db9507188b7ccf
liumapp <liumapp.com@gmail.com>
2019-03-22 14:04:55 +0800

    tag article done

--- data-transporter/src/main/java/com/liumapp/solo/transporter/services/impl/TagArticleHandler.java
+++ data-transporter/src/main/java/com/liumapp/solo/transporter/services/impl/TagArticleHandler.java
@@ -4,7 +4,10 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.liumapp.solo.transporter.contents.JsonFileContents;
 import com.liumapp.solo.transporter.db.entity.B3SoloTagArticle;
+import com.liumapp.solo.transporter.db.mapper.B3SoloTagArticleMapper;
 import com.liumapp.solo.transporter.services.HandDataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +24,13 @@ import java.util.Iterator;
 @Service
 public class TagArticleHandler implements HandDataService {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private JsonFileContents jsonFileContents;
+
+    @Autowired
+    private B3SoloTagArticleMapper mapper;
 
     @Override
     public void handle() {
@@ -30,8 +38,12 @@ public class TagArticleHandler implements HandDataService {
         B3SoloTagArticle b3SoloTagArticle = new B3SoloTagArticle();
         for (Iterator iterator = tagArticles.iterator(); iterator.hasNext();) {
             JSONObject tagArticle = (JSONObject) iterator.next();
-
+            b3SoloTagArticle.setOId(tagArticle.get("oId").toString());
+            b3SoloTagArticle.setArticleOid(tagArticle.get("article_oId").toString());
+            b3SoloTagArticle.setTagOid(tagArticle.get("tag_oId").toString());
+            mapper.insert(b3SoloTagArticle);
         }
+        logger.info("成功插入" + tagArticles.size() + "条标签与文章的关联数据");
     }
 
 }
